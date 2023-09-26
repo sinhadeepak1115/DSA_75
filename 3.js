ca244bc12f8540e88bd7be645698159286c3b967
shinhadeepak1115 <sinhadeepak1115@gmail.com>
2023-09-26 18:53:04 +0530

    sumZeros

---
+++ 3.js
@@ -0,0 +1,12 @@
+function sumZero(arr){
+    for (let i=0; i<arr.length; i++){
+        for(let j=i+1;j<arr.length; j++){
+            if(arr[i]+ arr[j]=== 0 ){
+                return [arr[i],arr[j]];
+            }
+        }
+    }
+}
+
+let ans = sumZero([-3,3,1,2,34,-34,])
+console.log(ans)
