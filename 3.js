49810614d4af50267f8c9ebbb887068f38a3f446
shinhadeepak1115 <sinhadeepak1115@gmail.com>
2023-09-26 18:59:10 +0530

    refactored 3.js

--- 3.js
+++ 3.js
@@ -10,3 +10,22 @@ function sumZero(arr){
 
 let ans = sumZero([-3,3,1,2,34,-34,])
 console.log(ans)
+
+// Better Refactored Solution
+
+function sumOfZeros(arr){
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right){
+        let sum =  arr[left] + arr[right];
+        if (sum === 0 ){
+            return [arr[left], arr[right]];
+        }else if (sum>0){
+            right --;
+        }else{
+            left ++;
+        }
+    }
+}
+let ans2 = sumOfZeros([-3,3,1,2,34,-34,])
+console.log(ans2)
